c4b08a585bacdeef731bd93377bac20f322c54fc
icardemil64 <icardemil64@gmail.com>
2021-02-02 22:42:05 -0300

    Modelos de escritores y libros agregados

---
+++ src/app/services/escritores.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {ESCRITORES} from '../db/escritores.db';
+import { Escritor } from '../models/escritor.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EscritoresService {
+
+  constructor() { }
+
+  getAll(): Escritor[] {
+    return ESCRITORES;
+  }
+}
